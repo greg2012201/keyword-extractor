038ea12b648f259c96ef8f2c8955dc92f99209e8
Grzegorz Dubiel <greg2012201@gmail.com>
2025-02-09 00:00:58 +0100

    Count schema when chunking text & add error handling.

--- src/getChunkedText.ts
+++ src/getChunkedText.ts
@@ -1,13 +1,46 @@
-import { encodingForModel } from "js-tiktoken";
+import { encodingForModel, Tiktoken } from "js-tiktoken";
 import { mainPrompt, systemPrompt } from "./prompts";
-import { MarkdownTextSplitter } from "@langchain/textsplitters";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import zodToJsonSchema from "zod-to-json-schema";
+import { keywordsResponseSchema } from "./schema";
+import { type ZodType } from "zod";
+
+function handleLimitError(
+    { calculatedChunkSize, schemaString }: { calculatedChunkSize: number; schemaString: string },
+    encoder: Tiktoken
+) {
+    if (calculatedChunkSize <= 0) {
+        throw new Error(
+            `maxChunkSize size is too small. Minimum maxChunkSize is ${
+                encoder.encode(systemPrompt).length +
+                encoder.encode(mainPrompt).length +
+                encoder.encode(schemaString).length
+            }`
+        );
+    }
+}
+
+function getSchemaAsString(schema: ZodType<any>) {
+    const jsonSchema = zodToJsonSchema(schema);
+
+    const schemaString = JSON.stringify(jsonSchema);
+
+    return schemaString;
+}
 
 async function getChunkedText(content: string, maxChunkSize: number = 2000) {
     const encoder = encodingForModel("gpt-4o-mini-2024-07-18");
+    const schemaString = getSchemaAsString(keywordsResponseSchema);
+    const calculatedChunkSize =
+        maxChunkSize -
+        encoder.encode(systemPrompt).length -
+        encoder.encode(mainPrompt).length -
+        encoder.encode(schemaString).length;
 
-    const chunkSize = maxChunkSize - encoder.encode(systemPrompt).length - encoder.encode(mainPrompt).length;
-    const textSplitter = new MarkdownTextSplitter({
-        chunkSize,
+    handleLimitError({ calculatedChunkSize, schemaString }, encoder);
+
+    const textSplitter = new RecursiveCharacterTextSplitter({
+        chunkSize: calculatedChunkSize,
 
         lengthFunction: (text: string) => encoder.encode(text).length,
     });
